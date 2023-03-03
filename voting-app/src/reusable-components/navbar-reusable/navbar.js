814f3d4b361b6d0ebe875d542bf185479437d920
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-03-03 17:35:43 +0100

    Button Reuseable Added

---
+++ voting-app/src/reusable-components/navbar-reusable/navbar.js
@@ -0,0 +1,11 @@
+const Navbar = ({text, ...props}) => {
+    return ( 
+        <div>
+            <ul>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Navbar;
